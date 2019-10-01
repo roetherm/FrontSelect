39a6a4a843102baf7f71b92c46b5719ee403fb96
Michael <michaelroether@gmx.net>
2019-10-01 20:52:28 +0200

    Initialized firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA5L2J8r6LX0fz0ztwK0SZuftHmw3kMKIc',
+    authDomain: 'thesis-aca6a.firebaseapp.com',
+    databaseURL: 'https://thesis-aca6a.firebaseio.com',
+    projectId: 'thesis-aca6a',
+    storageBucket: '',
+    messagingSenderId: '229208117486',
+    appId: '1:229208117486:web:eab097e39f4ab9ffe923ff'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA5L2J8r6LX0fz0ztwK0SZuftHmw3kMKIc',
+    authDomain: 'thesis-aca6a.firebaseapp.com',
+    databaseURL: 'https://thesis-aca6a.firebaseio.com',
+    projectId: 'thesis-aca6a',
+    storageBucket: '',
+    messagingSenderId: '229208117486',
+    appId: '1:229208117486:web:eab097e39f4ab9ffe923ff'
+  }
 };
 
 /*
